fe54178a2c2f2b4246a251a5f2ca51e1d34ab59a
msolisy <marisolsolis777@gmail.com>
2015-05-07 12:04:26 -0300

    Busca de rua

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -22,18 +22,38 @@ class PagesController extends BaseController {
 	
 	public function search()
 	{
+    //2015-05-06 msy begin, add param city
     $needle = Input::get("q");
+    $txtcity = Input::get("city"); 
+
 		if ($needle != "") {
       $tags = Tag::where('name', 'LIKE', '%' . $needle . '%')->get();
-      // photos
-      
-      $photos = Photo::where('name', 'LIKE', '%' . $needle . '%')
+       Log::info("Logging info txtcity <".$txtcity.">");
+       if ($txtcity != "") {
+
+        $allowed = "/[^a-z\\.\/\s]/i";
+        $txtstreet=  preg_replace($allowed,"",$needle);
+        $txtstreet = rtrim($txtstreet); 
+
+        //echo $txtstreet; die();
+          
+          Log::info("Logging info txtcity <".$txtcity.">"); 
+          $photos = Photo::where('city', 'LIKE', '%' . $txtcity . '%')
+                       ->where('street', 'LIKE', '%' . $txtstreet . '%')
+                       ->get();
+        $needle = $txtstreet;
+
+       }else{
+                $photos = Photo::where('name', 'LIKE', '%' . $needle . '%')
               ->orWhere('description', 'LIKE', '%' . $needle . '%')
               ->orWhere('imageAuthor', 'LIKE', '%' . $needle . '%')
               ->orWhere('workAuthor', 'LIKE', '%' . $needle . '%')
               ->orWhere('state', 'LIKE', '%' . $needle . '%')
               ->orWhere('city', 'LIKE', '%' . $needle . '%')
               ->get();
+
+       } 
+      //2015-05-06 msy end
       
       // se houver uma tag exatamente como a busca, pegar todas as fotos dessa tag e juntar no painel
       $tag = Tag::where('name', '=', $needle)->get();
@@ -42,42 +62,45 @@ class PagesController extends BaseController {
         $photos = $photos->merge($byTag);
       }
       // retorna resultado da busca
-      return View::make('/search',['tags' => $tags, 'photos' => $photos, 'query'=>$needle]);
+      return View::make('/search',['tags' => $tags, 'photos' => $photos, 'query'=>$needle, 'city'=>$txtcity]);
     } else {
       // busca vazia
-      return View::make('/search',['tags' => [], 'photos' => [], 'query' => ""]);
+      return View::make('/search',['tags' => [], 'photos' => [], 'query' => "", 'city'=>""]);
     }
 	}
   
   public function advancedSearch()
 	{
-    
+    //2015-05-06 msy begin, add workauthor
     $fields = array(
         'name',
         'description',
         'city',
         'state',
-        'country'
+        'country',
+        'workAuthor'
     );
     
     foreach($fields as $field) $$field = Input::get($field);
     
-    if(empty($name) && empty($description) && empty($city) && empty($state) && empty($country)) {
+    if(empty($name) && empty($description) && empty($city) && empty($state) && empty($country) && empty($workAuthor)) {
        // busca vazia
        return View::make('/advanced-search',['tags' => [], 'photos' => [], 'query' => ""]);
     } else {
       
       $query = Photo::where('id', '>', 0);
-      
+      //
       if ($name != '') $query->where('name', 'LIKE', '%'. $name .'%');  
       if ($description != '') $query->where('description', 'LIKE', '%'. $description .'%');  
       if ($city != '') $query->where('city', 'LIKE', '%'. $city .'%');  
       if ($state != '') $query->where('state', 'LIKE', '%'. $state .'%'); 
       if ($country != '') $query->where('country', 'LIKE', '%'. $country .'%');  
-      
+      if ($workAuthor != '') $query->where('workAuthor', 'LIKE', '%'. $workAuthor .'%'); 
+
       $photos = $query->get();
       
-    }
+    } //2015-05-06 msy end
+
     if($photos->count()) {
       // retorna resultado da busca
       return View::make('/advanced-search',['tags' => [], 'photos' => $photos]);
