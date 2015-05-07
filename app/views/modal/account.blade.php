3480dcbdb793b1fe597d94bf5d72ff8907963e5e
anapaula <anapaula_spbr@yahoo.com.br>
2015-05-07 00:16:56 -0300

    Inclusão de class para exibir mensagens de erro em vermelho

--- app/views/modal/account.blade.php
+++ app/views/modal/account.blade.php
@@ -19,36 +19,42 @@
           <div class="two columns alpha"><p>{{ Form::label('name', 'Nome*:') }}</p></div>
           <div class="two columns omega">
             <p>{{ Form::text('name') }} <br>
-            {{ $errors->first('name') }}</p>
+            <div class="error">{{ $errors->first('name') }} </div>
+            </p>
           </div>
           
           
           <div class="two columns alpha"><p>{{ Form::label('login', 'Login*:') }}</p></div>
           <div class="two columns omega">
             <p>{{ Form::text('login') }} <br>
-            {{ $errors->first('login') }}</p>
+            <div class="error">{{ $errors->first('login') }}</div>
+            </p>
           </div>
           
           <div class="two columns alpha"><p>{{ Form::label('email', 'E-mail*:') }}</p></div>
           <div class="two columns omega">
             <p>{{ Form::text('email') }}<br>
-            {{ $errors->first('email') }}</p>
+            <div class="error">{{ $errors->first('email') }}</div>
+            </p>
           </div>
           
           <div class="two columns alpha"><p>{{ Form::label('password', 'Senha*:') }}</p></div>
           <div class="two columns omega">
             <p>{{ Form::password('password') }}<br>
-            {{ $errors->first('password') }}</p>
+            <div class="error">{{ $errors->first('password') }}</div>
+            </p>
           </div>
           
-          <div class="two columns row alpha"><p>{{ Form::label('password_confirmation', 'Repita a senha*:') }}</p></div>
+          <!--<div class="two columns row alpha"><p>{{ Form::label('password_confirmation', 'Repita a senha*:') }}</p></div>
           <div class="two columns row omega"><p>{{ Form::password('password_confirmation') }}</p></div>
-          
+          -->
           <div class="four columns alpha omega">
           
             <p>Li e aceito os <a href="{{ URL::to('/termos') }}" target="_blank" style="text-decoration: underline;">termos de compromisso</a>: {{ Form::checkbox('terms', 'read') }}</p>
 			<p><a href="http://creativecommons.org/licenses/?lang=pt" id="creative_commons" style="text-decoration:underline;">Creative Commons</a></p>
-            <p>{{ $errors->first('terms') }}</p>
+            <p>
+              <div class="error">{{ $errors->first('terms') }}</div>
+            </p>
           
             <br>
             <p>{{ Form::submit("CADASTRAR", array('class'=>'btn right')) }}</p>
