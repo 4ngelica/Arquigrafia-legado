babe5333822f27c8d877268d711da939bda34268
msolisy <marisolsolis777@gmail.com>
2015-05-21 16:55:42 -0300

    Criação de text input para adição de filtros de busca avançada

--- app/views/advanced-search.blade.php
+++ app/views/advanced-search.blade.php
@@ -8,6 +8,13 @@
 <script src="{{ URL::to("/") }}/js/jquery.isotope.min.js"></script>
 
 <script type="text/javascript" src="{{ URL::to("/") }}/js/panel.js"></script>
+<!--Pickers -->
+<!--<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>-->
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
 @stop
 
@@ -19,38 +26,23 @@
       <div class="container">
         <div id="search_result" class="twelve columns row">
           
-          @if (true)
-          
-            <h1>Resultado da busca avançada</h1>
+          <!--@if(true)
             
-            <!-- TAGS
-            <p>Tags: 
+            <h1>Resultado da busca avançada</h1> -->           
             
-            @foreach($tags as $k => $tag)
-              
-              @if ($k != count($tags)-1 )
-              <a href="?q={{ $tag->name }}">{{ $tag->name }}</a>, 
-              @else
-              <a href="?q={{ $tag->name }}">{{ $tag->name }}</a>
-              @endif
-              
-            @endforeach
+            <?php // if ( count($photos) < 1 ) { ?>
+              <!-- <p>Não encontramos nenhuma imagem.</p> -->
+            <?php //} else { ?>
+              <!-- <p>Foram encontradas {{ count($photos) }} imagens.</p> -->
+            <?php //} ?>
             
-            </p>
-            -->
-            
-            <?php if ( count($photos) < 1 ) { ?>
-              <p>Não encontramos nenhuma imagem.</p>
-            <?php } else { ?>
-              <p>Foram encontradas {{ count($photos) }} imagens.</p>
-            <?php } ?>
-            
-          @else
+          <!--@else
             
             <h1>Busca avançada</h1>
             
-          @endif
-          
+          @endif-->
+
+          <h1>Busca avançada</h1>          
           <div class="twelve columns alpha">
           <p>Apenas os campos que forem preenchidos abaixo serão considerados na busca, para trazer as imagens que correspondam a todos os critérios informados.</p>
           </div>
@@ -64,6 +56,7 @@
             <h3>Descrição</h3>
             <p>{{ Form::label('name', 'Título da imagem:') }} {{ Form::text('name', Input::get("name") ) }}</p>
             <p>{{ Form::label('description', 'Descrição da imagem:') }} {{ Form::text('description', Input::get("description") ) }}</p>
+            <p>{{ Form::label('imageAuthor', 'Autor da imagem:') }} {{ Form::text('imageAuthor', Input::get("imageAuthor") ) }}</p>
           </div>
           
           <div class="four columns row">
@@ -76,7 +69,22 @@
           <div class="four columns omega row">
             <h3>Arquitetura</h3>
             <p>{{ Form::label('workAuthor', 'Arquiteto:') }} {{ Form::text('workAuthor', Input::get("workAuthor") ) }}</p>
+
+            <p>{{ Form::label('workdate', 'Data da obra:') }} 
+              {{ Form::text('workdate',Input::get("workdate"),array('id' => 'datePickerWorkDate')) }} </p>
+            <p>{{ Form::label('dataCriacao', 'Data da imagem:') }} 
+              {{ Form::text('dataCriacao',Input::get("dataCriacao"),array('id' => 'datePickerdataCriacao')) }} </p>
+            <p>{{ Form::label('dataUpload', 'Data de upload:') }} 
+              {{ Form::text('dataUpload',Input::get("dataUpload"),array('id' => 'datePickerdataUpload')) }} </p>
+            <!--<p>{{ Form::label('workdate', 'Data da obra:') }} {{ Form::text('workdate', Input::get("workdate") ) }}</p>-->
+            <!--<p>{{ Form::label('dataCriacao', 'Data da imagem:') }} {{ Form::text('dataCriacao', Input::get("dataCriacao") ) }}</p>-->
+            <!--<p>{{ Form::label('dataUpload', 'Data de upload:') }} {{ Form::text('dataUpload', Input::get("dataUpload") ) }}</p>-->
+            
+
+        
+
           </div>
+          
           <!-- 2015-05-06 msy end -->
           <div class="six columns alpha row">
             <p>{{ Form::submit('BUSCAR', ['class'=>'btn']) }}</p>
@@ -84,9 +92,19 @@
         
         </div>
         {{ Form::close() }}
-        
+
+        @if(count($photos) < 1)
+         <p>Não encontramos nenhuma imagem.</p> 
+        @elseif(count($photos) == 1) 
+          <p>Foi encontrada {{ count($photos) }} imagem.</p>
+        @else  
+          <p>Foram encontradas {{ count($photos) }} imagens.</p> 
+        @endif
       </div>
       
+      
+      
+
       <!--   PAINEL DE IMAGENS - GALERIA - CARROSSEL   -->  
       <div class="wrap">
         <div id="panel">
@@ -102,5 +120,32 @@
 	        
     </div>
     <!--   FIM - MEIO DO SITE   -->
+<script type="text/javascript">
+//msy
+    $(function() {
     
+    $( "#datePickerWorkDate" ).datepicker({
+      dateFormat:'dd/mm/yy',
+      keyboardNavigation: true,
+      orientation: "bottom right"
+    });
+    $( "#datePickerdataCriacao" ).datepicker({
+      dateFormat:'dd/mm/yy',
+      keyboardNavigation: true,
+      orientation: "bottom right"
+    });
+    $( "#datePickerdataUpload" ).datepicker({
+        dateFormat:'dd/mm/yy',
+      beforeShow: function(input, inst)
+        {
+
+        inst.dpDiv.css({marginTop: -65-input.offsetHeight + 'px', marginLeft: input.offsetWidth + 'px'});
+        }
+    });
+    
+
+    });
+</script> 
+
+
 @stop
