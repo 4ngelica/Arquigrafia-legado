a08481027bbedf6fdeb3c15d41dde9d92c88988d
msolisy <marisolsolis777@gmail.com>
2015-06-02 23:22:51 -0300

    Criação da função averageAndUserEvaluation para grafico de binomios

--- app/models/Evaluation.php
+++ app/models/Evaluation.php
@@ -26,6 +26,41 @@ class Evaluation extends Eloquent {
 			->groupBy('binomial_id')->get();
 	}
 
+	public static function averageAndUserEvaluation($photoId,$userId){
+
+			$avgPhotosBinomials = DB::table('binomial_evaluation')
+			->select('binomial_id', DB::raw('avg(evaluationPosition) as avgPosition'))
+			->where('photo_id', $photoId)
+			->orderBy('binomial_id', 'asc')
+			->groupBy('binomial_id')->get();
+			//dd($avgPhotosBinomials);
+	
+			$evaluations = null;
+			if($userId != null){
+			$evaluations = DB::table('binomial_evaluation')
+			->select('id','photo_id','evaluationPosition','binomial_id','user_id')
+			->where('user_id', $userId)
+			->where("photo_id", $photoId)
+			->orderBy("binomial_id", "asc")->get();
+			//$evaluations = Evaluation::where("user_id", $userId)
+			//->where("photo_id", $photoId)->orderBy("binomial_id", "asc")->get(); 
+			//$arrayEvaluation = $evaluations->toArray();
+
+			foreach ($evaluations as $valuesEvaluation) {
+				foreach ($avgPhotosBinomials as $avgBinomials) {
+					if($avgBinomials->binomial_id == $valuesEvaluation->binomial_id){
+						$valuesEvaluation->avg = $avgBinomials->avgPosition;
+						break;
+					}
+					
+				}
+			}
+
+		}
+			//dd($evaluations);
+			return $evaluations;			
+	}
+
 
 
 }
