cd2b1e18829052761ff66ae09dc602d654b0cc01
anapaula <anapaula_spbr@yahoo.com.br>
2015-06-04 11:15:48 -0300

    Correção dos textos da FAQ

--- app/views/faq.blade.php
+++ app/views/faq.blade.php
@@ -30,8 +30,11 @@
            <h3> Todas as imagens do Arquigrafia tem a mesma licença Creative Commons? </h3> 
            <p>Não, cada autor atribui uma licença específica às suas imagens, e pode mesmo atribuir licenças distintas para cada uma de suas imagens.</p>
         
-           <h3> Se eu fizer um download de uma imagem do Acervo da Biblioteca da FAU no Arquigrafia, como posso reconhecer esta imagem depois, em meio a outras nos meus arquivos pessoais? </h3> 
-           <p>Clicando com o botão direito do mouse sobre a imagem acesse ‘Propriedades’. Em seguida entre em ‘Detalhes’ e verifique a propriedade ‘Origem’, no qual constam os Direitos Autorais, e outros metadados.. A informação que consta neste campo irá mencionar FAU e depois a licença CC atribuída pelo autor ou por seus familiares, como por exemplo: FAU - Creative Commons 3.0 BY-NC-ND</p>
+           <h3>Se eu fizer um download de uma imagem do Acervo da Biblioteca da FAUUSP disponível no Arquigrafia, como posso reconhecer esta imagem depois, em meio a outras nos meus arquivos pessoais?</h3> 
+           <p>Clicando com o botão direito do mouse sobre a imagem acesse ‘Propriedades’. Em seguida entre em ‘Detalhes’ e verifique a propriedade ‘Origem’, no qual constam os Direitos Autorais, e outros metadados. A informação que consta neste campo irá mencionar FAU e depois a licença CC atribuída pelo autor ou por seus familiares, como por exemplo: FAU - Creative Commons 3.0 BY-NC-ND</p>
+
+           <h3>Posso solicitar a correção de alguma informação disponível no sistema que esteja incorreta?</h3> 
+           <p>Sim, certamente. Espera-se que a colaboração contínua dos usuários contribua para o aprimoramento e a correção das informações disponíveis no ARQUIGRAFIA. Para tanto, basta entrar em contato por e-mail com a equipe responsável pelo projeto no e-mail arquigrafiabrasil@gmail.com</p>
         
            <h3> Como faço para remover uma fotografia que enviei? </h3> 
            <p>Entre em seu perfil de usuário e em ‘Minhas imagens’ clique sobre a imagem que deseja remover. Assim que a imagem for ampliada clique no ícone ‘Remover foto’.</p>
@@ -50,7 +53,8 @@
       <div class="four columns">
         <h2>Ainda precisando de Ajuda?</h2>
 		<h3>Entre em contato com nossa equipe</h3>
-        <p> Esta é uma versão alpha do Arquigrafia e ainda pode conter erros. Caso tenha alguma dificuldade ou perceba problemas na navegação escreva para: <a href="mailto:arquigrafiabrasil@gmail.com " target="_blank">arquigrafiabrasil@gmail.com</a>.</p>
+        <p>Esta é uma versão alpha do Arquigrafia e ainda pode conter erros. Caso tenha alguma dificuldade técnica, dúvidas ou perceba problemas na navegação escreva para: <a href="mailto:arquigrafiabrasil@gmail.com " target="_blank">arquigrafiabrasil@gmail.com</a>.</p>
+
       </div>
       <!--   FIM - COLUNA DIREITA   -->
     </div>
