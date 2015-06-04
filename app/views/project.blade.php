7f0cdc7f27eeef6c5b9418ef95d0a38f48813091
anapaula <anapaula_spbr@yahoo.com.br>
2015-06-04 11:16:10 -0300

    Correção da lista de membros da equipe

--- app/views/project.blade.php
+++ app/views/project.blade.php
@@ -68,14 +68,15 @@
         <ul>
           <li><small><a href="http://lattes.cnpq.br/0999376629124379" target="_blank">Ana Paula Oliveira Bertholdo - Doutoranda IMEUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/3098839514190572" target="_blank">Aurelio Akira M. Matsui - Doutorando Universidade de Tokyo</a></small></li>
+          <li><small><a href="http://lattes.cnpq.br/1930659530940141" target="_blank">Gabriela Previdello Ferreira Orth - Doutoranda ECAUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/8776038016041917" target="_blank">Straus Michalsky - Doutorando IMEUSP</a></small></li>
-          <li><small><a href="http://lattes.cnpq.br/2605934447999302" target="_blank">José Teodoro - Mestrando IMEUSP</a></small></li>
+          <li><small><a href="http://lattes.cnpq.br/1225632464417175" target="_blank">Vladimir Emiliano Moreira Rocha - Doutorando PoliUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/3693915918069542" target="_blank">André Luís de Lima - Mestrando FAUUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/3965923523593288" target="_blank">Lucas Santos de Oliveira - Mestre IMEUSP</a> </small></li>
           <li><small><a href="http://lattes.cnpq.br/3873062445569152" target="_blank">Victor Williams Stafusa da Silva – Mestre IMEUSP</a></small> </li>
           <li><small><a href="http://lattes.cnpq.br/2350572278366806" target="_blank">Carlos Leonardo Herrera Muñoz - Mestrando IMEUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/2452505576333369" target="_blank">Edith Zaida Sonco Mamani - Mestranda IMEUSP</a></small></li>
-          <li><small><a href="http://lattes.cnpq.br/2605934447999302" target="_blank">José Teodoro - Mestrando IMEUSP</a> </small></li>
+          <li><small><a href="http://lattes.cnpq.br/2605934447999302" target="_blank">José Teodoro - Mestrando IMEUSP</a></small></li>           
           <li><small><a href="http://lattes.cnpq.br/0284126139128849" target="_blank">Laécio Freitas Chaves – Mestrando IMEUSP</a></small></li>
           <li><small><a href="#" target="_blank">André Jin Teh Chou – Graduando FAUUSP</a></small></li>
           <li><small><a href="#" target="_blank">Amanda Stenghel - Graduanda FAUUSP</a></small></li>
@@ -84,8 +85,7 @@
           <li><small><a href="#" target="_blank">Bruna Sellin Trevelin - Graduanda Direito USP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/0523942267027344" target="_blank">Diogo Augusto - Graduando em IC FAUUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/1387924249101546" target="_blank">Enzo Toshio S. L. de Mello - FITO</a></small></li>
-          <li><small><a href="http://lattes.cnpq.br/7486437542826471" target="_blank">Fernanda Adams Domingos - Graduanda FAUUSP</a></small></li>
-          <li><small><a href="http://lattes.cnpq.br/1930659530940141" target="_blank">Gabriela Previdello Ferreira Orth - Doutoranda ECAUSP</a></small></li>
+          <li><small><a href="http://lattes.cnpq.br/7486437542826471" target="_blank">Fernanda Adams Domingos - Graduanda FAUUSP</a></small></li>          
           <li><small><a href="http://lattes.cnpq.br/5529848738416350" target="_blank">Guilherme A. Nogueira Cesar - Graduando FAUUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/6605070572538127" target="_blank">Giuliano Salvatore Fiusa Magnelli - Graduando FAUUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/7968062612895669" target="_blank">Ilka Apocalypse Jóia Paulini - Graduanda FAUUSP</a></small></li>
@@ -96,11 +96,12 @@
           <li><small><a href="#" target="_blank">Kevin Ritschel - Graduando FAUUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/7328149928743537" target="_blank">Lucas Caracik - Graduando em IC FAUUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/0908900989509491" target="_blank">Luciana Molinari Monteforte - Graduanda FAUUSP</a></small></li>
-          <li><small><a href="http://lattes.cnpq.br/4891767216267666" target="_blank">Marisol Solis Yucra - Mestranda IMEUSP</a></small></li>
+          <li><small><a href="http://lattes.cnpq.br/5038915867638908" target="_blank">Marianna Kinuyo Kuraoka - Graduanda FAUUSP</a></small></li>
           <li><small><a href="#" target="_blank">Marina de Souza Barbosa Ferreira - Graduanda ECAUSP</a></small></li>
+          <li><small><a href="http://lattes.cnpq.br/4891767216267666" target="_blank">Marisol Solis Yucra - Mestranda IMEUSP</a></small></li>          
           <li><small><a href="http://lattes.cnpq.br/1280093125646759" target="_blank">Martim Passos - Graduando FAUUSP</a></small></li>
-          <li><small><a href="#" target="_blank">Nita Napoleão Encola - Graduanda ECAUSP</a></small></li>
-		  <li><small><a href="#" target="_blank">Renato Massao Maeda - Graduando IMEUSP</a></small></li>
+          <li><small><a href="http://lattes.cnpq.br/0439483635062120" target="_blank">Nita Napoleão Encola - Graduanda ECAUSP</a></small></li>
+		  <li><small><a href="#" target="_blank">Renato Massao Maeda da Silva - Graduando IMEUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/0223428961992896" target="_blank">Renato Veras de Paiva - Graduando ECAUSP</a></small></li>
           <li><small><a href="#" target="_blank">Rodrigo Ciorciari Salandim - Graduando EACHUSP</a></small></li>
           <li><small><a href="http://lattes.cnpq.br/8004254725476493" target="_blank">Ruth Cuiá Troncarelli - Graduanda FAUUSP</a></small></li>
@@ -113,12 +114,12 @@
         
         <h3>Parceiros de desenvolvimento</h3>
         <ul>
-			<li><small><a href="http://www.wikimedia.org/" target="_blank">Alexandre Hannud Abdo - Wikimedia</a></small></li>
-			<li><small><a href="http://www.rckt.com.br/" target="_blank">André Chou - RCKT</a></small></li>
-			<li><small><a href="http://www.bench.com.br/" target="_blank">Jean Pierre Chamouton - Benchmark Design Total</a></small></li>
-			<li><small><a href="http://www.rckt.com.br/" target="_blank">Pedro Emilio Guglielmo - RCKT</a></small></li>
-			<li><small><a href="http://www.brzcomunicacao.com.br/" target="_blank">Tiago Ortlieb - BRZ Comunicação</a></small></li>
-        </ul>
+          <li><small><a href="http://www.bench.com.br/" target="_blank">Jean Pierre Chamouton - Benchmark Design Total</a></small></li>
+          <li><small><a href="http://www.rckt.com.br/" target="_blank">Pedro Emilio Guglielmo - RCKT</a></small></li>
+          <li><small><a href="http://www.rckt.com.br/" target="_blank">André Chou - RCKT</a></small></li>
+          <li><small><a href="http://www.brzcomunicacao.com.br/" target="_blank">Tiago Ortlieb - BRZ Comunicação</a></small></li>
+			    <li><small><a href="http://www.wikimedia.org/" target="_blank">Alexandre Hannud Abdo - Wikimedia</a></small></li>
+			  </ul>
       </div>
       <!--   FIM - COLUNA DIREITA   -->
       
