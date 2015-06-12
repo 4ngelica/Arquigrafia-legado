406f640883404c25d94c03588990553d7b0102df
msolisy <marisolsolis777@gmail.com>
2015-06-11 23:37:23 -0300

    Create method to create directory to log and change function static

--- app/lib/utils/ActionUser.php
+++ app/lib/utils/ActionUser.php
@@ -6,44 +6,80 @@ use Carbon\Carbon;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LineFormatter;
+use Illuminate\Filesystem\Filesystem;
 
 class ActionUser{
 
-	public static function userEvents($user_id, $photo_id,$events,$sourcePage, $edit)
+	public function userEvents($user_id, $photo_id,$events,$sourcePage, $edit)
 	{
 		//to get occupation
         $time = Carbon::now('America/Sao_Paulo')->toDateTimeString();
+        $dateLog = date('Y-m-d');
         $arrayOccupation = Occupation::userOccupation($user_id);
         $arrayUsersRoles = UsersRole::valueUserRole($user_id);
         $stringOccupation ="";
         $stringRoles ="";
-        $stringOccupation = static::convertArrayObjectToString($arrayOccupation,'occupation');
-        $stringRoles = static::convertArrayObjectToString($arrayUsersRoles,'name');
-        //dd($stringRoles);
-        if (strcmp($edit, "edit") == 0 || strcmp($edit, "insertion") == 0) {
-            $log_info = sprintf('[%s][%d][%d][%s][%s][%s][%s]', $time, $user_id, $photo_id, $sourcePage, $stringOccupation, $stringRoles, $edit);
-        }
-        else {
-            $log_info = sprintf('Acesso do usuário [%d], com ocupação:[%s], e com role: [%s], acessou em :[%s],a partir da página [%s],e realizou as seguinte ação:[%s], da photo: [%d]',$user_id,$stringOccupation,$stringRoles,$time, $sourcePage,$events,$photo_id);     
-        } 
-        
-        $log = new Logger('Download_logger');
-        $file = storage_path() . '/logs/'.$events.'/'.$events.'.log';
-        if (!file_exists($file)) {
-          $handle = fopen($file, 'a+');
-          fclose($handle);
-        }
+        $stringOccupation = $this->convertArrayObjectToString($arrayOccupation,'occupation');
+        $stringRoles = $this->convertArrayObjectToString($arrayUsersRoles,'name');
 
-        $formatter = new LineFormatter("%message%\n", null, false, true);
-        $handler = new StreamHandler($file, Logger::INFO);
-        $handler->setFormatter($formatter);
-        $log->pushHandler($handler);
-        $log->addInfo($log_info);
+        $createResult = $this->createLogDirectory($dateLog,$events);
+
+        if($createResult){
+
+            if (strcmp($edit, "edit") == 0 || strcmp($edit, "insertion") == 0) {
+                 $log_info = sprintf('*Acesso do usuário [%d], com ocupação:[%s], e com role: [%s], acessou em :[%s],a partir da página [%s],e realizou as seguinte ação:[%s], da photo: [%d]',$user_id,$stringOccupation,$stringRoles,$time, $sourcePage,$edit,$photo_id);     
+            }
+             else {
+                 $log_info = sprintf('*Acesso do usuário [%d], com ocupação:[%s], e com role: [%s], acessou em :[%s],a partir da página [%s],e realizou as seguinte ação:[%s], da photo: [%d]',$user_id,$stringOccupation,$stringRoles,$time, $sourcePage,$events,$photo_id);     
+            } 
+        
+            $log = new Logger('Download_logger');
+            $file = storage_path() . '/logs/'.$dateLog.'/'.$events.'/'.'user_'.$user_id.'.log';
+            if (!file_exists($file)) {
+                $handle = fopen($file, 'a+');
+                fclose($handle);
+            }
+
+            $formatter = new LineFormatter("%message%\n", null, false, true);
+            $handler = new StreamHandler($file, Logger::INFO);
+            $handler->setFormatter($formatter);
+            $log->pushHandler($handler);
+            $log->addInfo($log_info);
+
+        }        
+
         return null; 
 
 	}
 
-    public static function convertArrayObjectToString($array,$atribute){
+
+    public function createLogDirectory($dateLog,$events){
+
+        $dateFile = storage_path() .'/logs/'.$dateLog;
+
+        $filesystem = new Filesystem();
+        if (!$filesystem->exists($dateFile)){
+            $resultDateFile = $filesystem->makeDirectory($dateFile);
+        }else{
+            $resultDateFile = true;
+        }     
+
+        $eventFile = storage_path() .'/logs/'.$dateLog.'/'.$events;
+
+        if (!$filesystem->exists($eventFile)){
+            $resultEventFile = $filesystem->makeDirectory($eventFile);
+        }else{
+            $resultEventFile = true;
+        }
+
+        if($resultDateFile == true && $resultEventFile == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public  function convertArrayObjectToString($array,$atribute){
 
         $string = "";
         $numElement = count($array);
@@ -60,6 +96,8 @@ class ActionUser{
         }
         return $string;
     }
+
+    //public static function
 
     //funções não utilizadas por enquanto, em desenvolvimento
 
