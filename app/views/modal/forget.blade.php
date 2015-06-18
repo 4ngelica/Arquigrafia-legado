62d6acccecd45c5076902ea6b6faa54608c142df
msolisy <marisolsolis777@gmail.com>
2015-06-18 16:25:38 -0300

    add line for validate email

--- app/views/modal/forget.blade.php
+++ app/views/modal/forget.blade.php
@@ -11,14 +11,23 @@
    <div class="container">
       <div class="registration">
          <!-- LOGIN RECUPERAR SENHA -->
+          
          @if($message == false)
          <div class="three columns offset-by-four">
-            <h1>Recuperação de senha</h1>            
+            <h1>Recuperação de senha</h1>  
+               @if($existEmail == false) 
+                  <p style="color:red"> <br/>
+                  O {{$email}} não existe no sistema.</br>
+                                 </p>
+               @endif
+
             {{ Form::open() }}
 
-               <p>{{ Form::label('forgot', 'Esqueceu sua senha?') }}</p>
+               <p>{{ Form::label('forgot', 'E-mail') }}</p>
                <br>               
-               <div class="two columns omega">{{ Form::text('email', '', array('class'=>'right','placeholder'=>'Insira seu e-mail') ) }}</div>
+               <div class="two columns omega">{{ Form::text('email', '', array('class'=>'right','placeholder'=>'Insira seu e-mail') ) }}
+                  <div class="error">{{ $errors->first('email') }} </div>
+               </div>
                <br>               
                <p>{{ Form::submit("Alterar Senha",array('class'=>'btn right')) }}</p>                           
                
@@ -44,8 +53,11 @@
                <p>&nbsp;</p> 
                <p>&nbsp;</p> 
          </div> 
-         @endif  
+         @endif 
+         
 
+         
+         
       </div>
    </div>
 
