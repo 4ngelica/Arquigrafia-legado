12a4645d095ea7a8cda315d49be13a0da7785c16
anapaula <anapaula_spbr@yahoo.com.br>
2015-06-24 13:49:39 -0300

    Inclusão de parâmetros para configuração de servidor de e-mail

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,8 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead'),
+	'homolog' => array('pcgerosa'),
+	'prod' => array('IME-001'),
 
 ));
 
