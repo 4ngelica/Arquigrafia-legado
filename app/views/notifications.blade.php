1ad1ce85a3ea37db19c394a123a784b7c910b2b5
raverona <raverona@hotmail.com>
2015-06-24 11:16:30 -0300

    Layout default na página de notificações

--- app/views/notifications.blade.php
+++ app/views/notifications.blade.php
@@ -1,11 +1,25 @@
-<?php
+@extends('layouts.default')
 
-$user = User::find(2);
+@section('head')
 
-?>
+<title>Arquigrafia - Notificações</title>
 
-<ul>
-    @foreach($user->notifications as $notification)
-    <li>{{ $notification->render() }}</li>
-    @endforeach
-</ul>
+@stop
+
+@section('content')
+		<?php
+
+		$user_id = Auth::user()->id;
+		$user = User::find($user_id);
+		$unreadNotifications = $user->notifications()->unread()->get();
+
+		?>
+
+		<ul>
+			<li>{{ "You have notifications!" }}</li>
+    		@foreach($user->notifications as $notification)
+    		<li>{{ $notification->render() }}</li>
+    		@endforeach
+		</ul>
+
+@stop
