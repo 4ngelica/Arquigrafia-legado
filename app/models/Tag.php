7a2295bbb8f0891f420c68298813afa0014f6ca2
msolisy <marisolsolis777@gmail.com>
2015-06-24 01:21:37 -0300

    funcction to recover photos by tags

--- app/models/Tag.php
+++ app/models/Tag.php
@@ -9,4 +9,25 @@ class Tag extends Eloquent {
 	public function photos() {
 		return $this->belongsToMany('Photo', 'tag_assignments', 'tag_id', 'photo_id');
 	}
+
+	public function photosTags($tags){
+		  $query = Tag::whereIn('name', $tags);
+		  $tagsResult = $query->get();  
+          $listTags= $tagsResult->lists('id');
+
+			
+				
+				$photosTagAssignment = DB::table('tag_assignments')
+				->select('photo_id')
+				->whereIn('tag_id',$listTags)				
+				->get(); 
+
+				$listPhotos=$photosTagAssignment->lists('photo_id');
+
+				$photos = Photo::wherein('id',$listPhotos);
+			
+				return $photos;
+
+		
+	}
 }
