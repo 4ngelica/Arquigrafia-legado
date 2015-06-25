65683ae783dcfc8eb398be1b079be9e6ee709fb0
raverona <raverona@hotmail.com>
2015-06-25 10:15:43 -0300

    Formatação das mensagens de notificação

--- app/lib/notifications/PhotoLikedNotification.php
+++ app/lib/notifications/PhotoLikedNotification.php
@@ -8,7 +8,13 @@ class PhotoLikedNotification extends \Tricki\Notification\Models\Notification
     public static $type = 'photo_liked';
 
     public function render() {
-        return 'this is a photo_liked notification';
+    	$created_at = $this->created_at;//object
+    	$object_type = $this->object_type;//string
+    	$object_id = $this->object_id;//integer
+    	$sender_id = $this->sender_id;//integer
+    	list($date, $time) = explode(" ", $created_at);//strings
+    	//$sender = User::find($sender_id);
+        return /*$sender . */"gostou da sua foto";
     }
 }
 
