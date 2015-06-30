3f538b1ab59246c599ea8cc2935bfcaadc9e4c00
msolisy <marisolsolis777@gmail.com>
2015-06-30 18:10:30 -0300

    Format of email and message to email of register

---
+++ app/views/emails/users/verify.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Bemvindo o Arquigrafia, {{$name}}</h2>
+
+        <div>
+             
+            Você deu o primeiro passo para ser parte de Arquigrafia, assim nós queremos tomar um momento
+            para pessoalmente dar a você as boas vindas. <br>
+
+            Nós pensamos que Arquigrafia é um bom lugar para compartilhar imagens arquitetônicas, e é 
+            excelente ter você a bordo!<br>
+
+            Por favor siga o link abaixo para confirmar o seu endereço de e-mail.<br/>  
+            {{ URL::to('users/register/' . $verifyCode) }}.<br>
+
+            Muito obrigado pelo seu tempo.        
+
+        </div>
+
+    </body>
+</html>
