ff5f2898e1174298e2c12262a5527cbcda53351c
msolisy <marisolsolis777@gmail.com>
2015-07-01 16:39:48 -0300

    called function to license

--- app/controllers/PhotosController.php
+++ app/controllers/PhotosController.php
@@ -45,15 +45,18 @@ class PhotosController extends \BaseController {
     }
     $source_page = Request::header('referer');
     ActionUser::printSelectPhoto($user_id, $id, $source_page, $user_or_visitor); 
-    $license = Photo::licensePhoto($photos); 
-
+    $license = Photo::licensePhoto($photos);
+   
     return View::make('/photos/show',
-      ['photos' => $photos, 'owner' => $photo_owner, 'follow' => $follow, 'tags' => $tags, 'commentsCount' => $photos->comments->count(), 'commentsMessage' => static::createCommentsMessage($photos->comments->count()),
+      ['photos' => $photos, 'owner' => $photo_owner, 'follow' => $follow, 'tags' => $tags, 
+      'commentsCount' => $photos->comments->count(), 
+      'commentsMessage' => static::createCommentsMessage($photos->comments->count()),
       'average' => $average, 'userEvaluations' => $evaluations, 'binomials' => $binomials, 
       'architectureName' => Photo::composeArchitectureName($photos->name),
       'similarPhotos'=>Photo::photosWithSimilarEvaluation($average,$photos->id),
       'photoliked' => $photoliked,
-      'license' =>$license      
+      'license' =>$license
+
       ]);
 	}
 	
