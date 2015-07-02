288f53dd656e3522882918457010376b7acba523
anapaula <anapaula_spbr@yahoo.com.br>
2015-07-02 14:57:28 -0300

    Descomentando like/dislike

--- app/views/photos/show.blade.php
+++ app/views/photos/show.blade.php
@@ -101,7 +101,7 @@ $(document).ready(function(){
                   @endif
                </span>
               <span class="right" title="{{ $commentsMessage }}"><i id="comments"></i> <small>{{$commentsCount}}</small></span>
-              <!--<span class="right" title="{{ $photos->likes->count() }} pessoas curtiram essa imagem"><i id="likes"></i> <small>{{$photos->likes->count()}}</small></span>-->
+              <span class="right" title="{{ $photos->likes->count() }} pessoas curtiram essa imagem"><i id="likes"></i> <small>{{$photos->likes->count()}}</small></span>
               
               <?php if (Auth::check() && Auth::user()->id == $photos->user_id) { ?>  
                	<span class="right">
@@ -132,12 +132,12 @@ $(document).ready(function(){
 
 					<li><a href="{{ URL::to('/photos/' . $photos->id . '/evaluate?f=sb' )}}" title="Avalie {{$architectureName}}" id="evaluate" ></a></li>  
             <!-- LIKE-->
-         <!--@if(is_null($photoliked))
+         @if(is_null($photoliked))
           <li> <a href="{{ URL::to('/photos/' . $photos->id . '/like' ) }}" id="like_button" title="Curtir"></a></li>
          @else
          <li> <a href="{{ URL::to('/photos/' . $photos->id . '/dislike' ) }}" id="like_button"
           class="dislike" title="Descurtir"></a></li>
-         @endif-->
+         @endif
 				</ul>            
              <?php } else { ?>
               <div class="six columns alpha">Faça o <a href="{{ URL::to('/users/login') }}">login</a> para fazer o download e comentar as imagens.</div>
@@ -181,9 +181,9 @@ $(document).ready(function(){
           <div class="text_comment" >
             <small>
               Cada usuário é responsável por seus próprios comentários. 
-            O Arquigrafia não se responsabiliza pelos      comentários postados,
+            O Arquigrafia não se responsabiliza pelos comentários postados,
              mas apenas por tornar indisponível no site o conteúdo considerado
-              infringente       ou danoso por determinação judicial (art.19 da Lei 12.965/14).
+              infringente ou danoso por determinação judicial (art.19 da Lei 12.965/14).
             </small>
             </div>
           <p></p>
@@ -237,17 +237,17 @@ $(document).ready(function(){
               <div class="four columns omega row">
                 <small id={{"$comment->id"}}>
                   {{$comment->user->name}} - {{$comment->created_at->format('d/m/Y h:m') }}
-                  <!--<img src="{{ URL::to("/") }}/img/commentNB.png" / ><small class='likes'>{{ $comment->likes->count()}}</small>-->
+                  <img src="{{ URL::to("/") }}/img/commentNB.png" / ><small class='likes'>{{ $comment->likes->count()}}</small>
                 </small>
                 <p>{{ $comment->text }}</p>
                 
                 <?php if (Auth::check()){ ?>
-                  <!--@if(!$comment->isLiked())
+                  @if(!$comment->isLiked())
 
                     <p> <a href="{{ URL::to('/comments/' . $comment->id . '/like' ) }}" class='like_comment' >Curtir</a></p>
                   @else
                     <p> <a href="{{ URL::to('/comments/' . $comment->id . '/dislike' ) }}" class='like_comment' class='dislike'>Descurtir</a></p>
-                  @endif-->
+                  @endif
                 <?php } ?>  
               </div>        
             </div>       
