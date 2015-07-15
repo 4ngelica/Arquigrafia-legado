8c287d0e91242439e8f31beb8245bf9e5d1370fc
raverona <raverona@hotmail.com>
2015-07-15 10:13:23 -0300

    Título do contador atualiza sem recarregar a página e trata singular e plural

--- app/views/notifications.blade.php
+++ app/views/notifications.blade.php
@@ -76,6 +76,9 @@
                 $.get(url)
                     .done(function( data ) {
                     var bubble = document.getElementById("bubble");
+                    var noteIcon = document.getElementById("notification");
+                    if (data != 1) noteIcon.title = "Você tem " + data + " notificações não lidas";
+                    else noteIcon.title = "Você tem " + data + " notificação não lida";
                     if (data > 0) bubble.innerHTML = data;
                     else bubble.style.display = "none";     
                 });
