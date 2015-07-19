a94659c503cbd6956f2747814be34623195dd0c6
rmmsilva <renato.massao@live.com>
2015-07-19 15:04:35 -0300

    Retirando gamificação 'feedback' do branch develop

--- app/views/photos/show.blade.php
+++ app/views/photos/show.blade.php
@@ -5,7 +5,6 @@
   <title>Arquigrafia - {{ $photos->name }}</title>
 
   <link rel="stylesheet" type="text/css" media="screen" href="{{ URL::to("/") }}/css/checkbox.css" />
-  <script type="text/javascript" src="{{ URL::to("/") }}/js/progressbar.js"></script>
   <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4fdf62121c50304d"></script>
   <!-- Google Maps API -->
   <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true"></script>
@@ -314,34 +313,6 @@
         @endif
       </hgroup>
 
-      @if ( $owner->equal(Auth::user()) )
-        <div id="image_info_completion">
-          <h4>Completude das informações:</h4>
-          <div id="progressbar"></div>
-          @if ($photos->information_completion < 100)
-            <p>
-              <a id="improve_image_data" href="{{ URL::to('/photos/' . $photos->id . '/edit') }}">Deseja melhorar as informações da imagem?</a>
-            </p>
-            <div id="information_input" class="four columns alpha omega row" >
-              <p><a href="#" class="close">X</a></p>
-              {{ Form::open( array('url' => 'photos/' . $photos->id . '/set/field') ) }}
-                <div class="four columns alpha omega">
-                  <h3>{{ $question }}</h3>
-                  {{ Form::hidden('field', $field) }}
-                  @if ($field == "description")
-                    <textarea name="description"></textarea>
-                  @else
-                    {{ Form::text($field, null) }}
-                  @endif
-                </div>
-                <input type="submit" class="btn" value="SALVAR">
-                <button class="btn" id="skip_question">PULAR</button>
-              {{ Form::close() }}
-              <img class="loader" src="{{ URL::to('/img/ajax-loader.gif') }}" />
-            </div>
-          @endif
-        </div>
-      @endif
       <div id="description_container">
       @if ( !empty($photos->description) )
         <h4>Descrição:</h4>
@@ -635,14 +606,6 @@
           }]
       });
     });
-  </script>
-  <script type="text/javascript">
-    var getFieldURL = "{{ $getFieldURL }}";
-    var setFieldURL = "{{ $setFieldURL }}";
-    var currentField = 1;
-    var hasField = true;
-    var progressbar = $('#progressbar').progressbar();
-    progressbar.progress( {{ $photos->information_completion }} );
   </script>
   
 @stop
