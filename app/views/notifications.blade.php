619d1ed0a743307b3ba87b39489662dfca76ae85
raverona <raverona@hotmail.com>
2015-07-23 09:06:04 -0300

    Refatoração para melhorar a formatação e os nomes das variáveis do código

--- app/views/notifications.blade.php
+++ app/views/notifications.blade.php
@@ -17,10 +17,10 @@
 	        $unreadNotifications = $user->notifications()->unread()->get();
             $readNotifications = $user->notifications()->read()->get();
 	?>
-	<h2 class="notifications">
+	<h2 class="notifications-title">
         Suas notificações:
         <div id="button-all-container">
-            <p id="read-all">Marcar todas como lidas:</p>
+            <p id="read-all-sentence">Marcar todas como lidas:</p>
             <div class="read-button" title="Marcar todas como lidas" onclick="readAll();"></div>
         </div>
     </h2>
@@ -37,7 +37,7 @@
     			<div id={{$notification->id}} class="notes<?php if($notification->read_at == null) echo ' not-read'?>" >
                     <li>
                         <div class="read-button" title="Marcar como lida" onclick="markRead(this);"></div>
-                        <div onclick="markRead(this);">
+                        <div class="notification-container" onclick="markRead(this);">
                             <a href={{"photos/" . $info_array[2]}}><img class="mini" src={{"/arquigrafia-images/" . $info_array[2] . "_home.jpg"}}></a>
                             @if($info_array[6] == null)
                             <a href={{"users/" . $info_array[5]}}>{{$info_array[1]}}</a>{{" curtiu sua " }} <a href={{"photos/" . $info_array[2]}}>{{"foto"}}</a>{{"."}}
@@ -45,7 +45,6 @@
                             <?php 
                                 $users = explode(":", $info_array[6]);
                                 $users_size = count($users) - 1;
-                                $names;
                                 for ($i = 0; $i < $users_size; $i++) {
                                     $user[$i] = User::find($users[$i+1]);
                                 }
@@ -53,18 +52,18 @@
                             @if($users_size < 2)
                             <a href={{"users/" . $info_array[5]}}>{{$info_array[1]}}</a>{{" e "}}<a href={{"users/" . $user[0]->id}}>{{$user[0]->name}}</a>{{" curtiram sua " }} <a href={{"photos/" . $info_array[2]}}>{{"foto"}}</a>{{"."}}
                             @else
-                            <a href={{"users/" . $info_array[5]}}>{{$info_array[1]}}</a>{{" e mais " }}<a class="fancybox" href={{"#users-" . $notification->id}}>{{$users_size . " pessoas"}}</a>{{" curtiram sua "}}<a href={{"photos/" . $info_array[2]}}>{{"foto"}}</a>{{"."}}
-                            <div class="users-note" id={{"users-" . $notification->id}}>
+                            <a href={{"users/" . $info_array[5]}}>{{$info_array[1]}}</a>{{" e mais "}}<a class="fancybox" href={{"#users-from-note-" . $notification->id}}>{{$users_size . " pessoas"}}</a>{{" curtiram sua "}}<a href={{"photos/" . $info_array[2]}}>{{"foto"}}</a>{{"."}}
+                            <div class="additional-users" id={{"users-from-note-" . $notification->id}}>
                                 <ul>
                                     @for($i = 0; $i < $users_size; $i++)
-                                    <li class="aditional-user"><a href={{"users/" . $user[$i]->id}}>{{$user[$i]->name}}</a></li>
+                                    <li class="additional-user"><a href={{"users/" . $user[$i]->id}}>{{$user[$i]->name}}</a></li>
                                     @endfor
                                 </ul>
                             </div>
                             @endif
                             @endif
                             </br>
-                            <p class="date">{{"$info_array[3], às $info_array[4]."}}</p>
+                            <p class="date-and-time">{{"$info_array[3], às $info_array[4]."}}</p>
                             <a class="link-block" href={{"photos/" . $info_array[2]}}></a>
                         </div>
                     </li>
