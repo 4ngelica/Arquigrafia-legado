23566e286060083e4648bd0be2d7070353dc1fab
raverona <raverona@hotmail.com>
2015-08-17 11:17:38 -0300

    Correção de sintaxe

--- app/views/users/edit.blade.php
+++ app/views/users/edit.blade.php
@@ -75,17 +75,15 @@
             </p>
           </div>
 
-          @if($user->login != $user->id_facebook) {
-            @if($user->login != $user->id_stoa) { 
+          @if($user->login != $user->id_facebook)
+            @if($user->login != $user->id_stoa)
               <div class="two columns alpha"><p>{{ Form::label('login', 'Login*:') }}</p></div>
               <div class="two columns omega">
                 <p>{{ Form::text('login', $user->login) }}<br>
                 <div class="error">{{ $errors->first('login') }} </div>
                 </p>
               </div>
-            }
             @endif
-          }
           @endif
           
           <div class="two columns alpha"><p>{{ Form::label('email', 'E-mail*:') }}</p></div>
