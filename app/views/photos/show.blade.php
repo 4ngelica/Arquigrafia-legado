a386feee4783dafab88b07ffeb63a4f0b196fc1a
msolisy <marisolsolis777@gmail.com>
2015-08-25 23:35:53 -0300

    flow to edit institutional form

--- app/views/photos/show.blade.php
+++ app/views/photos/show.blade.php
@@ -310,16 +310,17 @@
       <hgroup class="profile_block_title">
         <h3><i class="info"></i> Informações</h3>
           &nbsp; &nbsp;
-        @if ($owner->equal(Auth::user()))
-          @if(Session::get('institutionId'))
+          @if($belongInstitution)
           <a href= '{{"/photos/" . $photos->id . "/editInstitutional" }}' title="Editar informações da imagem">
           <img src="{{ asset("img/edit.png") }}" width="16" height="16"/>
           </a>
-          @else
+          @endif
+          @if($owner->equal(Auth::user()) && $hasInstitution == false && !Session::get('institutionId'))
+
           <a href= '{{"/photos/" . $photos->id . "/edit" }}' title="Editar informações da imagem">
           <img src="{{ asset("img/edit.png") }}" width="16" height="16"/>
           </a>
-          @endif
+        
         @endif
       </hgroup>
 
