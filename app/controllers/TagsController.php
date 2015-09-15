5ec3a624b9a0c34f0af7f91e0e5bebca3b4af3e1
raverona <raverona@hotmail.com>
2015-09-15 15:40:44 -0300

    Correção de bug: Busca por tags de imagens não deletadas

--- app/controllers/TagsController.php
+++ app/controllers/TagsController.php
@@ -8,4 +8,20 @@ class TagsController extends \BaseController {
 		return $tags;
 	}
 
+	public function refreshCount() {
+		$photos = Photo::all();
+		DB::update('update tags set count = 0');
+		foreach ($photos as $photo) {
+			$photo_tags = $photo->tags;
+			foreach ($photo_tags as $tag) {
+				$tag->count = $tag->count + 1;
+				$tag->save();
+			}
+		}
+		$deleted = Photo::onlyTrashed()->get();
+		foreach ($deleted as $photo) {
+			DB::table('tag_assignments')->where('photo_id', '=', $photo->id)->delete();
+		}
+	}
+
 }
