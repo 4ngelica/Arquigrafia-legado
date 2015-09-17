323e91e0b09bfa85d7b665319b3ee988f1f61edb
msolisy <marisolsolis777@gmail.com>
2015-09-17 18:01:01 -0300

    merge to perfil institution

--- app/controllers/InstitutionsController.php
+++ app/controllers/InstitutionsController.php
@@ -2,26 +2,42 @@
 
 class InstitutionsController extends \BaseController {
 
+  public function index()
+  {
+    $institution = Institution::all();
+    return $institution;
+  } 
+
   public function show($id) {
     $institution = Institution::find($id);
     if ( is_null($institution) ) {
       return Redirect::to('/');
     }
     $photos = $institution->photos()->get()->reverse();
-    // if (Auth::check()) {
-    //     $user_id = Auth::user()->id;
-    //     $user_or_visitor = "user";
-    // }
-    // else { 
-    //     $user_or_visitor = "visitor";
-    //     session_start();
-    //     $user_id = session_id();
-    // }
-    // $source_page = Request::header('referer');
-    // ActionUser::printSelectUser($user_id, $id, $source_page, $user_or_visitor);
+    /*
+    if(Session::has('institutionId')){
+        if(Session::get('institutionId') == $id)
+          $follow = false;
+        else
+          $follow = true;
+      }else{
+        $follow = true;
+      } 
 
-    //verificar seguir instituição
-    //log de ações
+       if(Session::has('institutionId')){
+            $institution_id = Session::get('institutionId');
+            $institution_or_visitor = "institution";
+        }else{
+            $institution_or_visitor = "visitor";
+            session_start();
+            $user_id = session_id();
+        }
+      
+    
+    //$source_page = Request::header('referer');
+    //ActionUser::printSelectUser($institution_id, $id, $source_page, $institution_or_visitor);
+
+      */
 
     return View::make('institutions.show', [
       'institution' => $institution, 'photos' => $photos,
