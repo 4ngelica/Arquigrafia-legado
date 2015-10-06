5e137f977d558317ce176755da9cf6e2aca8e688
anapaula <root@pcgerosa-dev.(none)>
2015-10-06 12:17:56 -0300

    Alteracao de quantidade de imagens no painel da Home

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -20,7 +20,7 @@ class PagesController extends BaseController {
 
 	public function home()
 	{ 
-    $photos = Photo::orderByRaw("RAND()")->take(1000)->get();
+    $photos = Photo::orderByRaw("RAND()")->take(350)->get();
 
     if (Auth::check()) {
       $user_id = Auth::user()->id;
@@ -44,7 +44,7 @@ class PagesController extends BaseController {
   
   public function panel()
 	{ 
-    $photos = Photo::orderByRaw("RAND()")->take(1000)->get();
+    $photos = Photo::orderByRaw("RAND()")->take(350)->get();
 		return View::make('api.panel', ['photos' => $photos]);
 	}
 
