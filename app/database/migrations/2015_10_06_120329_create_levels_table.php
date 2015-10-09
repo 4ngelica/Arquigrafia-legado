0d0bc9496cb4c946a2bf38508b55b82e6d999daf
rmmsilva <renato.massao@live.com>
2015-10-09 08:47:34 -0300

    Migrations para gamificação

--- app/database/migrations/2015_10_06_120329_create_levels_table.php
+++ app/database/migrations/2015_10_06_120329_create_levels_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class FillBadgesLikeTable extends Migration {
+class CreateLevelsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,11 @@ class FillBadgesLikeTable extends Migration {
 	 */
 	public function up()
 	{
-
+		Schema::create('levels', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+		});
 	}
 
 	/**
@@ -22,7 +26,7 @@ class FillBadgesLikeTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('levels');
 	}
 
 }
--- app/database/migrations/2015_10_06_142443_update_scores_table.php
+++ app/database/migrations/2015_10_06_142443_update_scores_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class FillBadgesLikeTable extends Migration {
+class UpdateScoresTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,8 @@ class FillBadgesLikeTable extends Migration {
 	 */
 	public function up()
 	{
-
+		DB::insert('insert into scores (points, user_id)' .
+			' (select 0, id from users where id not in (select user_id from scores))');
 	}
 
 	/**
