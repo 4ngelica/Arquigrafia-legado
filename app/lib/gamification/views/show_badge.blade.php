69cae286ab0e25cab5b79ab7515bdcaab4447dde
Renato Massao <renato.massao@live.com>
2015-10-23 12:16:34 -0200

    Alterando estilo do quadro de liderança e criando rota para visualizar badges

---
+++ app/lib/gamification/views/show_badge.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+@section('head')
+  <title>Arquigrafia - Seu universo de imagens de arquitetura</title>
+@stop
+
+@section('content')
+
+  <div class="container">
+    <div class="twelve columns">
+      <div class="six columns offset-by-three">
+        
+      </div>
+    </div>
+  </div>
+
+@stop
