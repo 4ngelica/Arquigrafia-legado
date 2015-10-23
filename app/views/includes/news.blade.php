67cf8380e9669cf36e8a15e6c5231232c27a7773
raverona <raverona@hotmail.com>
2015-10-23 14:31:15 -0200

    Mais definições para o feed de notícias

--- app/views/includes/news.blade.php
+++ app/views/includes/news.blade.php
@@ -84,13 +84,58 @@
   </div>
 </div>
 @elseif($info->news_type == 'edited_photo')<!--Alguém que você segue editou uma foto-->
-
+<div class="item h<?php echo $size; ?>">
+  <div class="layer" data-depth="0.2">
+    <a href='{{ URL::to("/photos") . "/" . $info->object_id }}'>
+    <?php 
+    ?>
+    <img data-src={{"/arquigrafia-images/" . $info->object_id . "_home.jpg"}} title="{{ Photo::find($info->object_id)->name }}">
+    </a>
+    <div class="item-title">
+      <p>{{User::find($info->sender_id)->name}} editou uma foto</p>
+    </div>
+  </div>
+</div>
 @elseif($info->news_type == 'edited_profile')<!--Alguém que você segue editou o perfil-->
-
+<div class="item h<?php echo $size; ?>">
+  <div class="layer" data-depth="0.2">
+    <a href='{{ URL::to("/users") . "/" . $info->object_id }}'>
+    <?php 
+    ?>
+    @if(User::find($info->object_id)->photo != null)
+      <img data-src={{"/arquigrafia-avatars/" . $info->object_id . ".jpg"}} title="{{ User::find($info->object_id)->name }}">
+    @else
+      <img data-src="{{ URL::to("/") }}/img/avatar-48.png" title="{{User::find($info->object_id)->name}}">
+    @endif
+    </a>
+    <div class="item-title">
+      <p>{{User::find($info->sender_id)->name}} editou seu perfil</p>
+    </div>
+  </div>
+</div>
 @elseif($info->news_type == 'highlight_of_the_week')<!--Destaque da semana-->
 
 @elseif($info->news_type == 'liked_photo')<!--Alguém que você segue gostou de uma foto-->
-
+<div class="item h<?php echo $size; ?>">
+  <div class="layer" data-depth="0.2">
+    <a href='{{ URL::to("/photos") . "/" . $info->object_id }}'>
+    <?php 
+    ?>
+    <img data-src={{"/arquigrafia-images/" . $info->object_id . "_home.jpg"}} title="{{ Photo::find($info->object_id)->name }}">
+    </a>
+    <div class="item-title">
+      @if($info->data == null)
+        <p>{{User::find($info->sender_id)->name}} gostou de uma foto</p>
+      @else
+      <?php 
+        $users = explode(":", $info->data);
+        $users_size = count($users) - 1;
+      ?>
+        <p>{{$users_size}} usuários gostaram de uma foto</p>
+      @endif
+    </div>
+  </div>
+</div>
 @endif
 
 
