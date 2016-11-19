21b1933245799d162c3bb84eb662988908bee89a
msolisy <marisolsolis777@gmail.com>
2016-11-19 21:44:39 -0200

    adicionando info::logs

--- app/modules/api/controllers/APIPhotosController.php
+++ app/modules/api/controllers/APIPhotosController.php
@@ -91,14 +91,17 @@ class APIPhotosController extends \BaseController {
 	        if ( !empty($input["photo_street"]) )
 	          $photo->street = $input["photo_street"];
 
-	      	if(!empty($input["workDate"])){             
+	      	\Log::info("workDate = ".$input["workDate"]);
+	      	if(!empty($input["workDate"])){    
+	      	   \Log::info("workDate inside if");         
                $photo->workdate = $input["workDate"];
                $photo->workDateType = "year";
            	}else{ 
                $photo->workdate = NULL;
             }
-
+            \Log::info("work author = ".$input["work_authors"]);
             if (\Input::has('work_authors')){
+            	\Log::info("work_authors inside if");
 	            $input["work_authors"] = str_replace(array('","'), '";"', $input["work_authors"]);    
 	            $input["work_authors"] = str_replace(array( '"','[', ']'), '', $input["work_authors"]);    
 	        }else $input["work_authors"] = ''; 
@@ -106,10 +109,12 @@ class APIPhotosController extends \BaseController {
 	        
             $author = new \Author();
             if (!empty($input["work_authors"])) {
+            	\Log::info("is going to save work_authors inside if");
                 $author->saveAuthors($input["work_authors"],$photo);
             }
-
-           	if(!empty($input["photo_imageDate"])){             
+            \Log::info("photo_imageDate = ".$input["photo_imageDate"]);
+           	if(!empty($input["photo_imageDate"])){   
+           		\Log::info("photo_imageDate inside if");          
                 $photo->dataCriacao = $this->date->formatDate($input["photo_imageDate"]);
                 $photo->imageDateType = "date";
             }else{ 
