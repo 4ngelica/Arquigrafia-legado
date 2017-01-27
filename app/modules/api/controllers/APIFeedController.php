d5fabc69f090e62c4d793fdd90b81670566692e4
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2017-01-27 17:04:24 -0200

    Corrigido draft no feed mobile

--- app/modules/api/controllers/APIFeedController.php
+++ app/modules/api/controllers/APIFeedController.php
@@ -42,7 +42,7 @@ class APIFeedController extends \BaseController {
 		$users_ids = $following_users->lists('id');
 		$institutions_ids = $following_institutions->lists('id');
 		$users_photos = \DB::table('photos')->whereNull('deleted_at')->whereNull('institution_id')->where('id', '<', $max_id)->whereIn('user_id', $users_ids);
-		$institutions_photos = \DB::table('photos')->whereNull('deleted_at')->where('id', '<', $max_id)->whereIn('institution_id', $institutions_ids);
+		$institutions_photos = \DB::table('photos')->whereNull('deleted_at')->whereNull('draft')->where('id', '<', $max_id)->whereIn('institution_id', $institutions_ids);
 		$all_photos = $institutions_photos->union($users_photos)->orderBy('created_at', 'desc')->take(20)->get();
 		$result = [];
 		foreach ($all_photos as $photo) {
