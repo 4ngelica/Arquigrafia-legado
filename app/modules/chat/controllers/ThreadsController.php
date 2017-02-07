1db9592f0126f457ef1dae37e12ac20a608ff0b6
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2017-02-07 09:26:58 -0200

    Adicionadas primeiras funcionalidades aos controladores

--- app/modules/chat/controllers/ThreadsController.php
+++ app/modules/chat/controllers/ThreadsController.php
@@ -14,23 +14,45 @@ use Illuminate\Support\Facades\View;
 
 class ThreadsController extends \BaseController {
 	public function test($id){
-		$user = \User::find($id);
-		return View::make('test', ['output' => $user->name]);
+		$thread = new Thread();
+		$thread->scopeForUser(\DB::table('users'), 1);
+		return View::make('test', ['output' => $thread]);
 	}
 
 	public function create($userId) {
+		$input = Input::all();
+		$thread = new Thread();
+		$thread->subject = $input['subject'];
+		$thread->save();
+
+		//tratamento de recebimento da lista de participantes, assumindo array
+
+		//usuario inicial
+		$participant = new Participant();
+		$participant->thread_id = $thread->id;
+		$participant->user_id = $userId;
+		$participant->last_read = Carbon::now();
+		$participant->save();
+
+		$thread->addParticipants($input['participants']);
+
+
 		return View::make('test', ['output' => 'create']);
 	}
 
 	public function destroy($userId) {
+		$input = Input::all();
 		return View::make('test', ['output' => 'destroy']);
 	}
 
 	public function index($userId) {
+		$thread = new Thread();
+		$thread->scopeForUser(\DB::table('users'), $userId);
 		return View::make('test', ['output' => 'index']);
 	}
 
 	public function show($userId, $chatId) {
-		return View::make('test', ['output' => 'show']);
+		$thread = Thread::find($chatId);
+		return View::make('test', ['output' => $thread]);
 	}
 }
