6942cc28f1a12ff43de63096d35b066d5d011fc4
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2017-06-13 23:01:17 -0300

    Finished adding project author to photos table

--- app/database/migrations/2017_06_13_155730_add_project_author_to_photos.php
+++ app/database/migrations/2017_06_13_155730_add_project_author_to_photos.php
@@ -12,7 +12,9 @@ class AddProjectAuthorToPhotos extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::table('photos', function(Blueprint $table){
+			$table->string('project_author')->default(null);
+		});
 	}
 
 	/**
@@ -22,7 +24,9 @@ class AddProjectAuthorToPhotos extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::table('photos', function($table){
+		  $table->dropColumn('project_author');
+		});
 	}
 
 }
