810b743fd8b9f9e9c838bb3c0dc264522553c3b1
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2017-06-14 16:52:12 -0300

    Added notification view for suggestion sent

--- app/views/includes/notes.blade.php
+++ app/views/includes/notes.blade.php
@@ -1,10 +1,10 @@
 <ul>
-    <?php $counter=0; ?>    
+    <?php $counter=0; ?>
         @foreach($user->notifications()->orderBy('created_at')->get()->reverse() as $notification)
             <?php
                 if($counter >= $max) break;
                 else $counter++;
-                $info_array = $notification->render(); 
+                $info_array = $notification->render();
                 if ( ! is_null($notification->deleted_at) ) {
                     continue;
                 }
@@ -21,7 +21,7 @@
                             @if($info_array[6] == null)
                             <a href={{URL::to('users/' . $info_array[5])}}>{{$info_array[1]}}</a>{{" curtiu sua " }} <a href={{URL::to('photos/' . $info_array[2])}}>{{"foto"}}</a>{{"."}}
                             @else
-                            <?php 
+                            <?php
                                 $users = explode(":", $info_array[6]);
                                 $users_size = count($users) - 1;
                                 for ($i = 0; $i < $users_size; $i++) {
@@ -56,7 +56,7 @@
                             @if($info_array[9] == null)
                             <a href={{URL::to("users/" . $info_array[5])}}>{{ $info_array[1]}}</a>{{" curtiu seu "}}<a href={{URL::to("photos/" . $info_array[2] . "#" . $info_array[8])}}>{{"comentário"}}</a>{{", na "}}<a href={{URL::to("photos/" . $info_array[2])}}>{{"foto"}}</a>{{" de "}}<a href={{URL::to("users/" . $info_array[6])}}>{{$info_array[7]}}</a>{{"."}}
                             @else
-                            <?php 
+                            <?php
                                 $users = explode(":", $info_array[9]);
                                 $users_size = count($users) - 1;
                                 for ($i = 0; $i < $users_size; $i++) {
@@ -91,7 +91,7 @@
                             @if($info_array[9] == null)
                             <a href={{URL::to("users/" . $info_array[5])}}>{{ $info_array[1]}}</a>{{" "}}<a href={{URL::to("photos/" . $info_array[2] . "#" . $info_array[8])}}>{{"comentou"}}</a>{{" sua "}}<a href={{URL::to("photos/" . $info_array[2])}}>{{"foto"}}</a>{{"."}}
                             @else
-                            <?php 
+                            <?php
                                 $users = explode(":", $info_array[9]);
                                 $users_size = count($users) - 1;
                                 for ($i = 0; $i < $users_size; $i++) {
@@ -132,7 +132,7 @@
                             @if($info_array[5] == null)
                             <a href={{URL::to("users/" . $info_array[4])}}>{{ $info_array[1]}}</a>{{" começou a seguir você."}}
                             @else
-                            <?php 
+                            <?php
                                 $users = explode(":", $info_array[5]);
                                 $users_size = count($users) - 1;
                                 for ($i = 0; $i < $users_size; $i++) {
@@ -164,13 +164,30 @@
                         <div class="read-button" title="Marcar como lida"  onclick="markRead(this);"></div>
                         <div onclick="markRead(this);">
                             <a href={{URL::to("badges/" . $info_array[3])}}>
-                                <img class="mini" 
+                                <img class="mini"
                                 src="{{ asset('img/badges/' . $info_array[4]) }}" >
                             </a>
                             Você ganhou o troféu
                             <a href={{URL::to("badges/" . $info_array[3])}}>"{{ $info_array[2]}}"</a>
                             </br>
                             <a class="link-block" href={{URL::to("badges/" . $info_array[3])}}></a>
+                        </div>
+                    </li>
+                </div>
+            @elseif($info_array[0] == "suggestionSent")
+                <div id={{$notification->id}} class="notes {{$notification->id}}<?php if($notification->read_at == null) echo ' not-read'?>">
+                    <li>
+                        <div class="read-button" title="Marcar como lida"  onclick="markRead(this);"></div>
+                        <div onclick="markRead(this);">
+
+                                <img class="mini"
+                                src="{{ URL::to("/") }}/img/avatar-48.png" >
+                            </a>
+                            Você teve sua sugestão enviada
+
+
+                            </br>
+
                         </div>
                     </li>
                 </div>
