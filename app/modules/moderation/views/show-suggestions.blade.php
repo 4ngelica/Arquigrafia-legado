096427fd41492375090f39d09d39a78ca98870d0
Kersul <joaohkfaria@gmail.com>
2017-08-10 21:23:05 -0300

    Implementing suggestions list

--- app/modules/moderation/views/show-suggestions.blade.php
+++ app/modules/moderation/views/show-suggestions.blade.php
@@ -2,6 +2,12 @@
 
 @section ('head')
   <title>Arquigrafia - Seu universo de imagens de arquitetura</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="{{ URL::to("/") }}/css/suggestions/suggestions-list.css" />
+
+  <script type="text/javascript">
+  // Missing fields and questions (to show on Modal)
+  var suggestions = {{ json_encode($suggestions) }};
+  </script>
 @stop
 
 @section ('content')
--- app/modules/moderation/views/suggestion_list.blade.php
+++ app/modules/moderation/views/suggestion_list.blade.php
@@ -1,20 +1,42 @@
 @foreach ($suggestions as $suggestion)
   <tr id="suggestion_{{ $suggestion['suggestion']->id }}" class="suggestion">
-    <td>{{ $suggestion['suggestion']->photo->name }}</td>
-    <td>{{ $suggestion['field'] }}</td>
-    <td></td>
-    <td>{{ $suggestion['suggestion']->text }}</td>
+    <!-- Image -->
     <td>
-      <form method="post">
-        <input name="suggestion_id" value="{{ $suggestion['suggestion']->id }}" type="hidden"/>
-        <input name="operation" value="rejected" type="hidden"/>
-        <input value="Rejeitar" type="submit"/>
-      </form>
-      <form method="post">
-        <input name="suggestion_id" value="{{ $suggestion['suggestion']->id }}" type="hidden"/>
-        <input name="operation" value="accepted" type="hidden"/>
-        <input  value="Aceitar" type="submit"/>
-      </form>
+      <a href="/photos/{{ $suggestion['suggestion']->photo->id }}" target="_blank">
+        <img class="suggestion_photo" src="/arquigrafia-images/{{ $suggestion['suggestion']->photo->id }}_home.jpg" />
+      </a>
+    </td>
+    <!-- Photo Name -->
+    <td>{{ $suggestion['suggestion']->photo->name }}</td>
+    <!-- Field -->
+    <td>{{ $suggestion['field_name'] }}</td>
+    <!-- Current Data -->
+    @if ($suggestion['suggestion']->photo[$suggestion['field']])
+      <td>{{ $suggestion['suggestion']->photo[$suggestion['field']] }}</td>
+    @else
+      <td>-----</td>
+    @endif
+    <!-- Suggested data -->
+    @if ($suggestion['suggestion']->text)
+      <td>{{ $suggestion['suggestion']->text }}</td>
+    @else
+      <td>-----</td>
+    @endif
+    <td>
+      <div class="suggestion-button thumbs-up">
+        <form id="send-thumbs-up-form" method="post">
+          <input name="suggestion_id" value="{{ $suggestion['suggestion']->id }}" type="hidden"/>
+          <input name="operation" value="accepted" type="hidden"/>
+          <a href="javascript:{}" onclick="document.getElementById('send-thumbs-up-form').submit(); return false;">Aceitar</a>
+        </form>
+      </div>
+      <div class="suggestion-button thumbs-down">
+        <form id="send-thumbs-down-form" method="post">
+          <input name="suggestion_id" value="{{ $suggestion['suggestion']->id }}" type="hidden"/>
+          <input name="operation" value="rejected" type="hidden"/>
+          <a href="javascript:{}" onclick="document.getElementById('send-thumbs-down-form').submit(); return false;">Rejeitar</a>
+        </form>
+      </div>
     </td>
   </tr>
 @endforeach
