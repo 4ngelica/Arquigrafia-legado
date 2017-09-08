4cbb1ecdbe805ffca55ff8b20c8f5a086ca5af6a
Kersul <joaohkfaria@gmail.com>
2017-09-08 14:42:59 -0300

    Adding REVOADA analytics and optimize

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -7,15 +7,19 @@
 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 })(window,document.documentElement,'async-hide','dataLayer',4000,
-{'GTM-53MPGDL':true});</script>
+// {'GTM-53MPGDL':true}); // PRODUCTION
+{'GTM-P4PGZX9':true}); // REVOADA
+</script>
 <!-- ANALYTICS AND OPTIMIZE -->
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-20571872-1', 'auto');
-  ga('require', 'GTM-53MPGDL');
+  // ga('create', 'UA-20571872-1', 'auto'); // PRODUCTION
+  ga('create', 'UA-106049892-1', 'auto'); // REVOADA
+  // ga('require', 'GTM-53MPGDL'); // PRODUCTION
+  ga('require', 'GTM-P4PGZX9'); // REVOADA
   ga('send', 'pageview');
 </script>
 <!--   ESTILO GERAL   -->
