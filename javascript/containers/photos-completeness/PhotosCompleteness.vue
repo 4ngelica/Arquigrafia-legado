68be21b356ac7d4685c92c576b4fcfc5447d3324
Your Name <you@example.com>
2017-12-06 18:45:14 -0200

    update

--- javascript/containers/photos-completeness/PhotosCompleteness.vue
+++ javascript/containers/photos-completeness/PhotosCompleteness.vue
@@ -31,16 +31,13 @@
   <div>
     <div class="container">
       <div id="search_result" class="twelve columns row">
-       <!-- <h1>O poder da colaboração: a força da inteligência coletiva!</h1>-->
-        <b>
-	<p>Ajude o Arquigrafia a ser uma fonte de conhecimento e informação sobre Arquitetura e seus Espaços Urbanos no mundo!</p>
+       <h3>Aprimorando o acervo</h3>
+	<p>Ajude o Arquigrafia a ser uma fonte de conhecimento sobre arquitetura e seus espaços urbanos!</p>
         <p>Você pode colaborar complementado os dados sobre as imagens.</p>
-        <p>Clique em "Ajude a completar dados" a partir da página de qualquer imagem compartilhada por usuários particulares do Arquigrafia.</p>
+        <p>Clique em "Ajude a completar dados" a partir de qualquer imagem compartilhada por usuários particulares do Arquigrafia.</p>
         <p>Questões vão auxiliar você a completar ou revisar os dados já inseridos.</p>
         <p>Juntos vamos aprimorar esse acervo de mais de 12300 imagens.</p>
-<!--<p>Comece agora mesmo a contribuir acessando uma imagem abaixo e ajude a aprimorar a qualidade das informações de nosso acervo!</p>-->
-<p> Contamos com você!</p>
-      </b>
+        <p>Comece agora mesmo a contribuir acessando uma imagem abaixo. Contamos com você!</p>
       </div>
     </div>
     <div v-if="store.state.isLoadingPhotos">
