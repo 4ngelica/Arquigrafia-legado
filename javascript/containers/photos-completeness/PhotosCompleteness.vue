88b946fa662292a05f75e033f369366773393263
Your Name <you@example.com>
2018-09-04 13:53:07 -0300

    Mensagem de repúdio

--- javascript/containers/photos-completeness/PhotosCompleteness.vue
+++ javascript/containers/photos-completeness/PhotosCompleteness.vue
@@ -29,29 +29,30 @@
 <template>
   <div>
     <div class="container">
-      <div class="twelve columns row">
+      <div class="twelve columns row" align="justify" >
         <h1>Nota de repúdio</h1>
-        <table bgcolor = "black">
+        <table bgcolor = "black" >
         <font color="white">
-        <p>Devido aos nossos esforços para preservar a herança e cultura brasileiras,</p>
-        <p>consideramos importante apresentar uma nota pública de repúdio aos cortes</p>
-        <p>orçamentários que comprometeram a manutenção do Museu Nacional no</p>
-        <p>Rio de Janeiro, levando ao incêndio que destruiu sua coleção de mais de 20</p>
-        <p>milhões de itens no dia 2/09/2018. É também para evitar perdas dessa</p>
-        <p>natureza que continuaremos trabalhando em colaboração.</p>
+        <p> Devido aos nossos esforços para preservar a herança e cultura
+        brasileiras, consideramos importante apresentar uma nota
+        pública de repúdio aos cortes orçamentários que comprometeram a manutenção do Museu Nacional no
+        Rio de Janeiro, levando ao incêndio que destruiu sua coleção de mais de 20
+        milhões de itens no dia 2/09/2018. É também para evitar perdas dessa
+          natureza que continuaremos trabalhando em colaboração.</p>
         </font>
         </table>
       </div>
 
-	
-      <div class="twelve columns row">
+
+ <div class="twelve columns row" align="justify">
       <h1>Colabore conosco na preservação do patrimônio cultural</h1>
-        <p>Ajude o Arquigrafia a ser uma fonte de conhecimento sobre arquitetura e seus espaços urbanos!</p>
-        <p>Você pode colaborar complementado os dados sobre as imagens.</p>
-        <p>Clique em "Ajude a completar dados" a partir de qualquer imagem compartilhada por usuários particulares do Arquigrafia.</p>
-        <p>Questões vão auxiliar você a completar ou revisar os dados já inseridos.</p>
-        <p>Juntos vamos aprimorar esse acervo de mais de 12300 imagens.</p>
-        <p>Comece agora mesmo a contribuir acessando uma imagem abaixo. Contamos com você!</p> 
+        <!--<p>Ajude o Arquigrafia a ser uma fonte de conhecimento sobre arquitetura e seus espaços urbanos!-->
+	<p>Você tem imagens sobre o Museu Nacional? Compartilhe conosco!! Vamos manter a memória desse importante patrimônio!
+        Você também pode colaborar completando os dados sobre as imagens.
+        Clique em "Ajude a completar dados" a partir de qualquer imagem compartilhada por usuários particulares do Arquigrafia.
+        Questões vão auxiliar você a completar ou revisar os dados já inseridos.
+        Juntos vamos aprimorar esse acervo de mais de 12000 imagens.
+        Comece agora mesmo a contribuir acessando uma imagem abaixo. Contamos com você!</p> 
       </div>
     </div>
     <div v-if="store.state.isLoadingPhotos">
