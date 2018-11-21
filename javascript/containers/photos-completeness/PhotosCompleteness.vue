49ca2e5a4299f92d8e249bf6b1b8b59218848135
Your Name <you@example.com>
2018-11-21 11:59:57 -0200

    Atualizacoes

--- javascript/containers/photos-completeness/PhotosCompleteness.vue
+++ javascript/containers/photos-completeness/PhotosCompleteness.vue
@@ -29,16 +29,17 @@
 <template>
   <div>
     <div class="container">
-      <div class="twelve columns row" align="justify" >
+      <div class="twelve columns row" style="margin-right:30px; margin-left=30px">
         <h1>Nota de repúdio</h1>
-        <table bgcolor = "black" >
+        <table bgcolor = "black" style="width:100%">
         <font color="white">
-        <p> Devido aos nossos esforços para preservar a herança e cultura
+        <p style="line-height: 1.5" align="justify"> Devido aos nossos esforços para preservar a herança e cultura
         brasileiras, consideramos importante apresentar uma nota
         pública de repúdio aos cortes orçamentários que comprometeram a manutenção do Museu Nacional no
         Rio de Janeiro, levando ao incêndio que destruiu sua coleção de mais de 20
         milhões de itens no dia 2/09/2018. É também para evitar perdas dessa
           natureza que continuaremos trabalhando em colaboração.</p>
+        <p style="line-height: 1.5"> </p>
         </font>
         </table>
       </div>
@@ -47,12 +48,13 @@
  <div class="twelve columns row" align="justify">
       <h1>Colabore conosco na preservação do patrimônio cultural</h1>
         <!--<p>Ajude o Arquigrafia a ser uma fonte de conhecimento sobre arquitetura e seus espaços urbanos!-->
-	<p>Você tem imagens sobre o Museu Nacional? Compartilhe conosco!! Vamos manter a memória desse importante patrimônio!
+	<p style="line-height: 1.5">Você tem imagens sobre o Museu Nacional? Compartilhe conosco!! Vamos manter a memória desse importante patrimônio!
         Você também pode colaborar completando os dados sobre as imagens.
         Clique em "Ajude a completar dados" a partir de qualquer imagem compartilhada por usuários particulares do Arquigrafia.
         Questões vão auxiliar você a completar ou revisar os dados já inseridos.
         Juntos vamos aprimorar esse acervo de mais de 12000 imagens.
         Comece agora mesmo a contribuir acessando uma imagem abaixo. Contamos com você!</p> 
+      <p style="line-height: 1.5"> </p>
       </div>
     </div>
     <div v-if="store.state.isLoadingPhotos">
